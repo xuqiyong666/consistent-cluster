e720f1f446c9f32ddda5422319c7f3af80610dd5
jeffrey6052 <jeffrey6052@163.com>
2016-04-29 13:48:40 +0800

    release v1.0.4

--- lib/consistent-cluster/sync-client.rb
+++ lib/consistent-cluster/sync-client.rb
@@ -11,6 +11,8 @@ module ConsistentCluster
 
     def initialize(options)
 
+      @syncingMutex = Mutex.new
+
       @data = {}
       @cluster = {}
       @node_register = {}
@@ -20,10 +22,19 @@ module ConsistentCluster
 
       @create_proc = options[:create_proc]
       @destroy_proc = options[:destroy_proc]
-      @after_sync_proc = options[:after_sync_proc] 
+      @after_sync_proc = options[:after_sync_proc]
+      @log_proc = options[:log_proc]
 
-      @to_sync,@syncing = false,false
-      
+      @logger = if options[:log_file]
+        Logger.new(options[:log_file])
+      elsif STDERR || STDOUT
+        Logger.new(STDERR || STDOUT)
+      else
+        Logger.new(options[:nil])
+      end
+
+      @last_sync_at = 0
+
       @path = options[:zookeeper_path]
 
       @zk = ZK.new(options[:zookeeper_service],reconnect: true)
@@ -65,47 +76,81 @@ module ConsistentCluster
       server
     end
 
+    def invoke_sync
+      sync_services
+    end
+
+    def local_version
+      @data
+    end
+
+    def remote_version
+      app_names = get_app_names(watch: false)
+
+      data = {}
+      app_names.each do |app_name|
+        app_content = get_app_content(app_name, watch: false)
+        data[app_name] = app_content
+      end
+      data
+    end
+
+    def zookeeper_path
+      @path
+    end
+
+    def last_sync_at
+      @last_sync_at
+    end
+
     protected
 
     def sync_services
-      @to_sync = true
-      if !@syncing
+      @syncingMutex.synchronize do
         syncing_process
       end
     end
 
     def syncing_process
-      @syncing = true
-      while @to_sync
-        @to_sync = false
-        app_names = get_app_names
-        current_app_names = @cluster.keys
+      app_names = get_app_names
+      current_app_names = @cluster.keys
 
-        to_update = current_app_names&app_names
-        to_create = app_names - current_app_names
-        to_destroy = current_app_names - app_names
+      to_update = current_app_names&app_names
+      to_create = app_names - current_app_names
+      to_destroy = current_app_names - app_names
 
-        to_update.each do |app_name|
-          update_service(app_name)
-        end
-
-        to_create.each do |app_name|
-          create_service(app_name)
-        end
-
-        to_destroy.each do |app_name|
-          destroy_service(app_name)
-        end
+      to_update.each do |app_name|
+        update_service(app_name)
       end
-      @syncing = false
-      if @after_sync_proc
+
+      to_create.each do |app_name|
+        create_service(app_name)
+      end
+
+      to_destroy.each do |app_name|
+        destroy_service(app_name)
+      end
+
+      @last_sync_at = Time.now.to_i
+
+      if @after_sync_proc || @log_proc
         clone_data = Marshal.load(Marshal.dump(@data)) #avoid change outside
-        @after_sync_proc.call(clone_data)
+
+        if @after_sync_proc
+          @after_sync_proc.call(clone_data)
+        end
+
+        if @log_proc
+          debugInfo = @log_proc.call(@data).to_s
+          if debugInfo != ""
+            @logger.info debugInfo
+          end
+        end
       end
     end
 
-    def get_app_names
-      @zk.children(@path, watch: true)
+    def get_app_names(options = {watch: true})
+      @zk.children(@path, watch: options[:watch])
     end
 
     def create_service(app_name)
@@ -130,7 +175,7 @@ module ConsistentCluster
       end
 
     rescue Exception => boom
-      puts "sync create :#{app_name} raise #{boom.class} - #{boom.message}"
+      @logger.error "syncClientError when create :#{app_name} raise #{boom.class} - #{boom.message}"
     end
 
     def destroy_service(app_name)
@@ -151,7 +196,7 @@ module ConsistentCluster
       end
 
     rescue Exception => boom
-      puts "sync destroy :#{app_name} raise #{boom.class} - #{boom.message}"
+      @logger.error "syncClientError when destroy :#{app_name} raise #{boom.class} - #{boom.message}"
     end
 
     def update_service(app_name)
@@ -166,12 +211,12 @@ module ConsistentCluster
         create_service(app_name)
       end
     rescue Exception => boom
-      puts "sync update :#{app_name} raise #{boom.class} - #{boom.message}"
+      @logger.error "syncClientError when update :#{app_name} raise #{boom.class} - #{boom.message}"
     end
 
-    def get_app_content(app_name)
+    def get_app_content(app_name, options={watch: true})
       app_path = "#{@path}/#{app_name}"
-      content = @zk.get(app_path, watch: true).first
+      content = @zk.get(app_path, watch: options[:watch]).first
       content
     end
 
